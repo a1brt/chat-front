576f7c513996b0b864caa55687eae529ce356237
Ablrt <albert_amadanyan@edu.aua.am>
2023-10-13 23:03:42 +0400

    chat improvements

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,38 +1,42 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-key */
-import { Box, Container, TextField, Button } from "@mui/material";
+import { Box, Container, TextField, Button, Typography } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { useEffect, useRef, useState } from "react";
 
 const footerSize = 64;
 
 function Chat(props) {
-  const myRef = useRef(null);
+  const curRef = useRef(null);
+  function Message(props) {
+    return (
+      <Box p={1} mb={1}>
+        <Typography
+          width="100%"
+          height={48}
+          bgcolor="#90caf9"
+          boxShadow={1}
+          autoFocus
+          ref={curRef}
+          variant="h5"
+          p={1}
+        >
+          {props.text}
+        </Typography>
+      </Box>
+    );
+  }
   const [messages, setMessages] = useState([]);
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState("");
   useEffect(() => {
     const m = props.chatData.messages.map((item) => {
-      return (
-        <Box p={1} mb={1}>
-          <Box
-            width="100%"
-            height={48}
-            bgcolor="#90caf9"
-            key={item}
-            boxShadow={1}
-            autoFocus
-            ref={myRef}
-          >
-            {item}
-          </Box>
-        </Box>
-      );
+      return <Message text={item} />;
     });
     setMessages(m);
   }, []);
 
   useEffect(() => {
-    myRef.current.scrollIntoView();
+    curRef.current.scrollIntoView();
   }, [messages]);
 
   const containerStyles = {
@@ -52,32 +56,18 @@ function Chat(props) {
   };
   const footerStyles = { display: "flex", eight: footerSize };
   const handleSend = () => {
-    const m = (
-      <Box p={1} mb={1}>
-        <Box
-          width="100%"
-          height={48}
-          bgcolor="#90caf9"
-          key={1}
-          boxShadow={1}
-          autoFocus
-          ref={myRef}
-        >
-          {message}
-        </Box>
-      </Box>
-    );
-    setMessages([...messages, m]);
+    if (message) setMessages([...messages, <Message text={message} />]);
   };
 
-
   return (
-    <Container ref={myRef} sx={{ ...containerStyles }}>
+    <Container ref={curRef} sx={{ ...containerStyles }}>
       <Box sx={{ ...chatBoxStyles }}>{messages}</Box>
       <Box sx={{ ...footerStyles }}>
         <TextField
           sx={{ ...messageInputStyles }}
-          onChange={(e)=>{setMessage(e.target.value)}}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
           variant="outlined"
           margin="normal"
           required
